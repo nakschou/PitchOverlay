08624ff4e86008f96a9870ec88e367e1c5447427
Justin Chou <87676813+nakschou@users.noreply.github.com>
2023-05-09 15:06:24 -0700

    modifications

--- vid_prediction.py
+++ vid_prediction.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 import numpy as np
 import cv2 as cv
-import json
+import pandas as pd
 
 model_path = 'runs/detect/pitch_detection_v12/weights/best.pt'
 vid_path = 'pitcher_vids/pitcher (1).mp4'
-boxes_path = 'boxes.txt'
-confidence_ind = 5
+boxes_path = 'boxes.csv'
+confidence_ind = 4
+num_elems = 5
 
 def get_boxes(model, vid_path):
     cap = cv.VideoCapture(vid_path)
@@ -21,13 +22,16 @@ def get_boxes(model, vid_path):
         results = model.predict(source=image, save=True)
         #Create boxes dictionary
         no_boxes = len(results[0].boxes.xyxy)
-        boxes_dct[counter] = np.array(no_boxes)
-        for i in range(no_boxes): #Populate boxes dictionary
+        boxes_dct[counter] = np.zeros((no_boxes, num_elems))
+        for i in range(no_boxes): #For every box
+            #Gets length of coordinates array
             len_coords_arr = len(results[0].boxes.xyxy[i])
-            coords_w_conf = np.array(len_coords_arr+1)
+            #Creates a new array of coordinates with confidence
+            coords_w_conf = np.zeros(len_coords_arr+1)
+            #Populates the new array
             for j in range(len_coords_arr):
                 coords_w_conf[j] = results[0].boxes.xyxy[i][j]
-            coords_w_conf[confidence_ind] = results[0].boxes.conf[i][0]
+            coords_w_conf[confidence_ind] = results[0].boxes.conf[i].item()
             boxes_dct[counter][i] = coords_w_conf
         counter += 1
         if cv.waitKey(1) == ord("q"):
@@ -36,13 +40,9 @@ def get_boxes(model, vid_path):
     cv.destroyAllWindows()
     return boxes_dct
 
-def dict_to_json(dict, file_loc):
-    with open(file_loc, 'w') as convert_file:
-     convert_file.write(json.dumps(dict))
-
-
 if __name__ == "__main__":
     # Loads the model, change pathing based on what you need
     model = YOLO(model_path)
     boxes_dct = get_boxes(model, vid_path)
-    dict_to_json(boxes_dct, boxes_path)
+    df = pd.DataFrame(boxes_dct)
+    df.to_csv(boxes_path, index=False)
